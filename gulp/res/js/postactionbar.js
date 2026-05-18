d754b1ed81b0a55c5687e0a75fd01f6c1363f6de
s5lachlan <s5s5s5wum@gmail.com>
2026-05-18 15:15:16 +0100

    Updates and fixes

--- gulp/res/js/postactionbar.js
+++ gulp/res/js/postactionbar.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	//let postActionButtonContainer = document.getElementById("post-action-buttons")
 	let postActionBarClose = document.getElementById("post-action-bar-close")
+	let postActionBarLabelText = document.getElementById("post-action-bar-label-text")
 	let postActionBar = document.getElementById("post-action-bar")
 	let emojiButton = document.getElementById("emoji-button")
 	
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	
 	
 	function emojiButtonClicked() {
+		postActionBarLabelText.textContent = "Emojis"
 		postActionBarShow()
 	}
 	
