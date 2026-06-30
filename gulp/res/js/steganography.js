fdbfcc9f4494effaa47af2e64b0c0685eed4a9f2
s5lachlan <s5s5s5wum@gmail.com>
2026-06-30 11:28:37 +0100

    fix new post bug

--- gulp/res/js/steganography.js
+++ gulp/res/js/steganography.js
@@ -100,7 +100,9 @@ const handleSteg = (e) => {
 	//add the remoderation toggle link and event listener
 	if (!e.detail.hover) {
 		const stegButtons = e.detail.post.querySelectorAll('.steganography-link');
-		const stegSlider = e.detail.post.closest(".post-file").querySelectorAll(".steganography-slider");
+		console.log(e.detail.post)
+		const stegSlider = e.detail.post.querySelectorAll(".steganography-slider");
+		console.log(stegSlider)
 		for (let i = 0; i < stegButtons.length; i++) {
 			stegButtons[i].addEventListener('click', onStegClicked, false);
 		}
